4f3aec356dd9aecc487e5c15bd9e8edf935d1eb1
Victor Cassone <vic.cassone@gmail.com>
2025-08-12 10:26:56 -0500

    ainitalize SDK

--- product-lucra-gyp/android-app/app/src/main/java/com/lucra/android/MainActivity.kt
+++ product-lucra-gyp/android-app/app/src/main/java/com/lucra/android/MainActivity.kt
@@ -1,16 +1,54 @@
 package com.lucra.android
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.navigation.compose.rememberNavController
 import com.lucra.android.navigation.AppNavHost
+import com.lucrasports.sdk.core.LucraClient
+import com.lucrasports.sdk.core.style_guide.ClientTheme
+import com.lucrasports.sdk.core.style_guide.ColorStyle
+import com.lucrasports.sdk.core.ui.LucraFlowListener
+import com.lucrasports.sdk.core.ui.LucraUiProvider
+import com.lucrasports.sdk.ui.LucraUi
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        LucraClient.initialize(
+            apiKey = "coXydksUigTnn87Z6e45tabTSOaTBj0l",
+            apiUrl = "api-rng.sandbox.lucrasports.com",
+            environment = LucraClient.Companion.Environment.SANDBOX,
+            lucraUiProvider = LucraUi(
+                lucraFlowListener = object : LucraFlowListener {
+                    override fun launchNewLucraFlowEntryPoint(entryLucraFlow: LucraUiProvider.LucraFlow): Boolean {
+                        Log.d("Sample", "launchNewLucraFlowEntryPoint: $entryLucraFlow")
+                        return true
+                    }
+
+                    override fun onFlowDismissRequested(entryLucraFlow: LucraUiProvider.LucraFlow) {
+                        Log.d("Sample", "onFlowDismissRequested: $entryLucraFlow")
+
+                    }
+                }
+            ),
+            customLogger = null,
+            outputLogs = true,
+            clientTheme = ClientTheme(
+                lightColorStyle = ColorStyle(
+                    primary = "#ec4899",
+                    secondary = "#ec4899",
+                ),
+                darkColorStyle = ColorStyle(
+                    primary = "#ec4899",
+                    secondary = "#ec4899",
+                ),
+            ),
+            application = application
+        )
         // Restore any previously logged in user before building the UI
         UserManager.init(applicationContext)
         setContent {
